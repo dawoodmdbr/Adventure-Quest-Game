48808896824629c4b1587971c14c7d5f6bdc54ac
Mian Dawood Bin Rafay <dawoodbinrafaydbr@gmail.com>
2025-04-25 00:29:54 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 const int MAX_SIZE = 11;
 
-// Forward declaration
 class Player;
 
 class Item {
@@ -60,18 +59,32 @@ private:
     int points;
     int x, y;
     int gold, silver;
-    Item *inventory[10];
-    int inventoryCount = 0;
+    int boardSize;
+    bool directionDown;
+    bool goalReached = false;
 
 public:
-    Player(string n, int startX, int startY)
-        : name(n), x(startX), y(startY), points(0), gold(10), silver(20) {
+    Player(string n, int startX, int startY, int bSize, bool down)
+        : name(n), x(startX), y(startY), points(0), gold(10), silver(20), boardSize(bSize), directionDown(down) {
         cout << "Player " << name << " created at (" << x << ", " << y << ")\n";
     }
 
     void move() {
-        // Basic right move for now
-        if (y + 1 < MAX_SIZE) y++;
+        if (goalReached) return;
+    
+        int rowIdx = directionDown ? (boardSize - 1 - x) : x;
+        if (rowIdx % 2 == 0) {
+            if ((directionDown && y < boardSize - 1) || (!directionDown && y > 0))
+                y += directionDown ? 1 : -1;
+            else
+                x += directionDown ? -1 : 1;
+        } else {
+            if ((directionDown && y > 0) || (!directionDown && y < boardSize - 1))
+                y += directionDown ? -1 : 1;
+            else
+                x += directionDown ? -1 : 1;
+        }
+    
         cout << name << " moved to (" << x << ", " << y << ")\n";
     }
 
@@ -80,25 +93,27 @@ public:
         cout << name << " now has " << points << " points\n";
     }
 
-    void addGold(int g) {
-        gold += g;
+    void addGold(int g) { 
+        gold += g; 
+        cout<< name << "collected a gold coin.\n";
     }
-
-    void addSilver(int s) {
-        silver += s;
+    void addSilver(int s) { 
+        silver += s; 
+        cout << name << "collected a silver coin.\n";
     }
 
     string getName() const { return name; }
     int getX() const { return x; }
     int getY() const { return y; }
+    int getPoints() const { return points; }
+    char getSymbol() const { return toupper(name[0]); }
+    bool hasReachedGoal() const { return goalReached; }
 };
 
 void coin::interact(Player &player) {
     player.updatePoints(value);
-    if (isGold)
-        player.addGold(1);
-    else
-        player.addSilver(1);
+    if (isGold) player.addGold(1);
+    else player.addSilver(1);
 }
 
 void HelperObject::interact(Player &player) {
@@ -130,10 +145,14 @@ public:
         }
     }
 
-    void display() {
+    void display(Player *p1, Player *p2) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (grid[i][j] == nullptr)
+                if (p1->getX() == i && p1->getY() == j)
+                    cout << p1->getSymbol() << " ";
+                else if (p2->getX() == i && p2->getY() == j)
+                    cout << p2->getSymbol() << " ";
+                else if (grid[i][j] == nullptr)
                     cout << "_ ";
                 else
                     cout << grid[i][j]->getName()[0] << " ";
@@ -162,13 +181,13 @@ private:
     Player *player1;
     Player *player2;
     int currentTurn;
-    int level;
+    bool gameEnded = false;
 
 public:
-    AdventureQuest(int size)
-        : board(size), currentTurn(1), level(1) {
-        player1 = new Player("Player1", 0, 0);
-        player2 = new Player("Player2", size - 1, 0);
+    AdventureQuest(int size, const string &p1Name, const string &p2Name)
+        : board(size), currentTurn(1) {
+        player1 = new Player(p1Name, size - 1, 0, size, true);
+        player2 = new Player(p2Name, 0, size - 1, size, false);
         placeInitialItems();
     }
 
@@ -188,10 +207,12 @@ public:
 
     void displayGameStatus() {
         cout << "\n=== Board ===\n";
-        board.display();
+        board.display(player1, player2);
     }
 
     void playTurn() {
+        if (gameEnded) return;
+
         Player *current = (currentTurn % 2 == 1) ? player1 : player2;
         cout << "\nTurn " << currentTurn << ": " << current->getName() << "'s turn\n";
         current->move();
@@ -204,13 +225,36 @@ public:
 
         currentTurn++;
 
+        if (current->getX() == board.getSize() / 2 && current->getY() == board.getSize() / 2) {
+            gameEnded = true;
+            cout << "\n=== CENTER REACHED! GAME OVER ===\n";
+            cout << current->getName() << " has reached the center and triggered the end!\n";
+        
+            int p1Points = player1->getPoints();
+            int p2Points = player2->getPoints();
+        
+            cout << player1->getName() << "'s points: " << p1Points << "\n";
+            cout << player2->getName() << "'s points: " << p2Points << "\n";
+        
+            if (p1Points == p2Points)
+                cout << "It's a draw!\n";
+            else if (p1Points > p2Points)
+                cout << player1->getName() << " wins!\n";
+            else
+                cout << player2->getName() << " wins!\n";
+        
+            return;
+        }
     }
 
-    void runGame(int turns = 10) {
-        for (int i = 0; i < turns; i++) {
-            displayGameStatus();
+    void runGame(int turns = 100) {
+        displayGameStatus();
+        system("pause");
+        while (!gameEnded) {
+            system("cls"); 
             playTurn();
-            system("pause"); 
+            displayGameStatus();
+            system("pause");
         }
     }
 
@@ -221,7 +265,14 @@ public:
 };
 
 int main() {
-    AdventureQuest game(5); // 5x5 board
-    game.runGame(10);       // Play 10 turns
+    string name1, name2;
+    cout << "Enter Player 1 Name: ";
+    getline(cin, name1);
+    cout << "Enter Player 2 Name: ";
+    getline(cin, name2);
+
+    AdventureQuest game(5, name1, name2);
+    game.runGame();
+
     return 0;
 }
